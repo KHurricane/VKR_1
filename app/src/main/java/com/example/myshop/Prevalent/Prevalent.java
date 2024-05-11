dafd11b1e2cee8fb70ce447e1ef0f2aefb9ef310
KHurricane <haritonow.egor2014@yandex.ru>
2024-05-12 00:18:35 +0300

    Initial commit

---
+++ app/src/main/java/com/example/myshop/Prevalent/Prevalent.java
@@ -0,0 +1,12 @@
+package com.example.myshop.Prevalent;
+
+import com.example.myshop.Model.Users;
+
+public class Prevalent {
+    public static Users currentOnlineUser = new Users("33","33","33","com.google.android.gms.tasks.zzw@edc5c4a");
+
+    public static final String UserPhoneKey="UserPhone";
+    public static final String UserPasswordKey="UserPassword";
+
+
+}
